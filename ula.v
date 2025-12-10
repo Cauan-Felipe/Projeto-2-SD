acd2649d5a5c7cc06cb1abf422faefbe4e4bf10d
Cauan-Felipe <cfsc@cin.ufpe.br>
2025-12-10 18:56:41 -0300

    Add files via upload

---
+++ ula.v
@@ -0,0 +1,25 @@
+module ula (
+    input signed [5:0] A, B,
+    input [2:0] param,
+    output reg [6:0] S
+);
+
+
+    always @(*) begin
+        case (param)
+            //Soma
+            3'b001: S = A + B; //ADD   
+            3'b010: S = A + B; //ADDI
+
+            //Subtração
+            3'b011: S = A - B; //SUB
+            3'b100: S = A - B; //SUBI
+
+            //Multiplicação
+            3'b101: S = A * B; //MUL
+
+            default: S = 6'b0;
+        endcase
+    end
+    
+endmodule
